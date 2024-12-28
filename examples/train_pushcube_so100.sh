237b9525f23986dfb294c83ebc6f26b596942754
Joosep Pata <joosep.pata@gmail.com>
2024-12-28 18:30:00 +0200

    added examples to train on so100 sim

---
+++ examples/train_pushcube_so100.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+export PYTHONPATH=./:../gym_so100
+export HYDRA_FULL_ERROR=1
+
+export MUJOCO_GL=glfw
+JOB_NAME=tdmpc_so100_kp
+
+
+python lerobot/scripts/train.py \
+    hydra.job.name=$JOB_NAME \
+    hydra.run.dir=outputs/train/$(date +'%Y-%m-%d/%H-%M-%S')_${JOB_NAME} \
+    env=so100 \
+    env.gym.observation_mode=state \
+    env.gym.render_mode=rgb_array \
+    policy=tdmpc_pushcube_so100 \
+    training.log_freq=1 \
+    training.eval_freq=10000 \
+    training.offline_steps=0 \
+    training.online_steps=1000 \
+    training.online_rollout_n_episodes=10 \
+    training.online_rollout_batch_size=10 \
+    training.online_steps_between_rollouts=100 \
+    training.online_buffer_capacity=40000 \
+    training.online_buffer_seed_size=0 \
+    training.online_sampling_ratio=1.0 \
+    training.do_online_rollout_async=false  \
+    training.save_freq=50000 \
+    training.save_checkpoint=true \
+    training.num_workers=1 \
+    eval.batch_size=50 \
+    eval.n_episodes=50 \
+    eval.use_async_envs=false \
+    device=mps \
+    use_amp=true \
+    resume=false
