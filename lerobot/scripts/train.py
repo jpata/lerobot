f2474f08b4546b6928575f6216aeeefa06f4acc7
Joosep Pata <joosep.pata@cern.ch>
2025-01-07 12:08:01 +0200

    working version

--- lerobot/scripts/train.py
+++ lerobot/scripts/train.py
@@ -594,21 +594,21 @@ def train(cfg: DictConfig, out_dir: str | None = None, job_name: str | None = No
 
             return online_rollout_s, update_online_buffer_s
 
-        # online_rollout_s, update_online_buffer_s = sample_trajectory_and_update_buffer()
+        online_rollout_s, update_online_buffer_s = sample_trajectory_and_update_buffer()
         # If we aren't doing async rollouts, or if we haven't yet gotten enough examples in our buffer, wait
         # here until the rollout and buffer update is done, before proceeding to the policy update steps.
-        future = executor.submit(sample_trajectory_and_update_buffer)
-        if (
-            not cfg.training.do_online_rollout_async
-            or len(online_dataset) <= cfg.training.online_buffer_seed_size
-        ):
-            online_rollout_s, update_online_buffer_s = future.result()
+        # future = executor.submit(sample_trajectory_and_update_buffer)
+        # if (
+        #     not cfg.training.do_online_rollout_async
+        #     or len(online_dataset) <= cfg.training.online_buffer_seed_size
+        # ):
+        #     online_rollout_s, update_online_buffer_s = future.result()
 
-        if len(online_dataset) <= cfg.training.online_buffer_seed_size:
-            logging.info(
-                f"Seeding online buffer: {len(online_dataset)}/{cfg.training.online_buffer_seed_size}"
-            )
-            continue
+        # if len(online_dataset) <= cfg.training.online_buffer_seed_size:
+        #     logging.info(
+        #         f"Seeding online buffer: {len(online_dataset)}/{cfg.training.online_buffer_seed_size}"
+        #     )
+        #     continue
 
         policy.train()
         for _ in range(cfg.training.online_steps_between_rollouts):
@@ -652,10 +652,10 @@ def train(cfg: DictConfig, out_dir: str | None = None, job_name: str | None = No
 
         # If we're doing async rollouts, we should now wait until we've completed them before proceeding
         # to do the next batch of rollouts.
-        if future.running():
-            start = time.perf_counter()
-            online_rollout_s, update_online_buffer_s = future.result()
-            await_update_online_buffer_s = time.perf_counter() - start
+        # if future.running():
+        #     start = time.perf_counter()
+        #     online_rollout_s, update_online_buffer_s = future.result()
+        #     await_update_online_buffer_s = time.perf_counter() - start
 
         if online_step >= cfg.training.online_steps:
             break
